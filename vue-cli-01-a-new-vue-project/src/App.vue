9cc518e91e9292b79ee9f092dbd9a6e8f463b1e9
Arthur (DPA) <a.foadjo-dje@psychologenakademie.de>
2024-04-10 15:34:07 +0200

    Creating a Basic App

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <section>
+        <h2>My Friends</h2>
+        <ul v-for="friend in friends">
+            <li>Name: {{ friend.name }}</li>
+            <li>Phone: {{ friend.phone }}</li>
+            <li>E-Mail: {{ friend.email }}</li>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            friends: [
+                {
+                    id: "1",
+                    name: "Jean Tabi",
+                    phone: "+237 689 888 888",
+                    email: "jt@yahoo.com"
+                },
+                {
+                    id: "2",
+                    name: "Marie Ange",
+                    phone: "+237 674 888 888",
+                    email: "mariea@yahoo.com"
+                }
+            ]
+        };
+    }
+};
+</script>
