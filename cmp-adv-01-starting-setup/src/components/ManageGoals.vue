6b8c18fd1f9568305f2488d970ec6dde186043c3
Arthur (DPA) <a.foadjo-dje@psychologenakademie.de>
2024-04-12 16:17:29 +0200

    Applying what we know

--- cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,6 +1,38 @@
 <template>
     <div>
         <h2>Manage Goals</h2>
-        <input type="text" />
+        <input type="text" ref="goal" />
+        <button @click="setGoal">Set Goal</button>
+        <error-alert v-if="isValidInput">
+            <h2>Input is invaid!</h2>
+            <p>Please enter at least a few character...</p>
+            <button type="button" @click="confirmError">Okay</button>
+        </error-alert>
     </div>
 </template>
+
+<script>
+import ErrorAlert from './ErrorAlert.vue';
+
+export default {
+    components: {
+        ErrorAlert
+    },
+    data() {
+        return {
+            isValidInput: false
+        };
+    },
+    methods: {
+        setGoal() {
+            const enteredValue = this.$refs.goal.value;
+            if (enteredValue === '') {
+                this.isValidInput = !this.isValidInput;
+            }
+        },
+        confirmError() {
+            this.isValidInput = !this.isValidInput;
+        }
+    }
+};
+</script>
