ed2301a4d6f3e4c08b03924f90e4950e1ac8e3cd
Arthur (DPA) <a.foadjo-dje@psychologenakademie.de>
2024-04-13 08:48:19 +0200

    Create frist component to display learning resources

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <ul>
+        <ResourceItem v-for="resource in storedResources" :key="resource.id"
+            :title="resource.title"
+            :description="resource.description"
+            :link="resource.link"
+        />
+    </ul>
+</template>
+
+<script>
+import ResourceItem from './components/learning-resources/ResourceItem.vue';
+
+export default {
+    components: {
+        ResourceItem
+    },
+    data() {
+        return {
+            storedResources: [
+                {
+                    id: 'official-guide', 
+                    title: 'Official Guide', 
+                    description: 'The official Vue.js documentation.',
+                    link: 'https://vuejs.org'
+                },
+                {
+                    id: 'google', 
+                    title: 'google', 
+                    description: 'Learn to google.',
+                    link: 'https://googgle.org'
+                }
+            ]
+        };
+    }
+};
+</script>
