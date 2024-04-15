822237b32dd308ff1d80550528a45295da3f0b66
Arthur (DPA) <a.foadjo-dje@psychologenakademie.de>
2024-04-15 15:52:25 +0200

    Adding a basic vaalidation

--- forms-01-starting-setup/src/components/TheForm.vue
+++ forms-01-starting-setup/src/components/TheForm.vue
@@ -1,8 +1,9 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div class="form-control" :class="{ invalid: userNameValidity === 'invalid' }">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" v-model="userName" />
+      <input id="user-name" name="user-name" type="text" v-model.trim="userName" @blur="validateInput" />
+      <p v-show="userNameValidity === 'invalid'">Please enter a valid name!</p>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
@@ -65,7 +66,8 @@ export default {
       referrer: 'wom',
       interest: [],
       how: null,
-      confirm: false
+      confirm: false,
+      userNameValidity: 'pending'
     };
   },
   methods: {
@@ -82,6 +84,13 @@ export default {
       this.how = null;
       console.log(this.confirm);
       this.confirm = false;
+    },
+    validateInput() {
+      if (this.userName === '') {
+        this.userNameValidity = 'invalid';
+      } else {
+        this.userNameValidity = 'valid';
+      }
     }
   }
 };
@@ -132,6 +141,14 @@ input[type='radio'] {
 input[type='checkbox'] + label,
 input[type='radio'] + label {
   font-weight: normal;
+}
+
+.form-control.invalid input {
+  border-color: red;
+}
+
+.form-control.invalid label {
+  border-color: red;
 }
 
 button {
