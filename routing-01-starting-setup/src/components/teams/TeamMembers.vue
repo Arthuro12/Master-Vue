f9767f3904ec71968efee4305a57b6a06d72dd6d
Arthur (DPA) <a.foadjo-dje@psychologenakademie.de>
2024-04-19 10:37:34 +0200

    Passing parameters as props

--- routing-01-starting-setup/src/components/teams/TeamMembers.vue
+++ routing-01-starting-setup/src/components/teams/TeamMembers.vue
@@ -20,6 +20,7 @@ export default {
   components: {
     UserItem
   },
+  props: ['teamId'],
   inject: ['users', 'teams'],
   data() {
     return {
@@ -28,8 +29,7 @@ export default {
     };
   },
   methods: {
-    loadTeamMembers(route) {
-      const teamId = route.params.teamId;
+    loadTeamMembers(teamId) {
       const selectedTeam = this.teams.find((currTeam) => currTeam.id === teamId);
       const members = selectedTeam.members;
       const selectedMembers = [];
@@ -42,11 +42,11 @@ export default {
     }
   },
   created() {
-    this.loadTeamMembers(this.$route);
+    this.loadTeamMembers(this.teamId);
   },
   watch: {
-    $route(newRoute) {
-      this.loadTeamMembers(newRoute);
+    teamId(newId) {
+      this.loadTeamMembers(newId);
     }
   }
 };
