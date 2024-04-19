f5f8ad9388b6f2e40031296987baa7f9ce744113
Arthur (DPA) <a.foadjo-dje@psychologenakademie.de>
2024-04-19 10:51:39 +0200

    Redirecting and catch all routes

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+import NotFound from './components/nav/NotFound.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
 import UsersList from './components/users/UsersList.vue';
@@ -9,14 +10,17 @@ import UsersList from './components/users/UsersList.vue';
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+        { path: '/', redirect: '/teams' },
+        // { path: '/teams', component: TeamsList, alias: '/' },
         { path: '/teams', component: TeamsList },
         { path: '/users', component: UsersList },
-        { path: '/teams/:teamId', component: TeamMembers, props: true }
+        { path: '/teams/:teamId', component: TeamMembers, props: true },
+        { path: '/:notFound(.*)', component: NotFound }
     ],
     linkActiveClass: 'active' 
 });
 
-const app = createApp(App)
+const app = createApp(App);
 
 app.use(router);
 
