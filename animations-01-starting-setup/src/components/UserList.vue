d21687264e600b721a4a446fdba2edf1bae26e67
Arthur (DPA) <a.foadjo-dje@psychologenakademie.de>
2024-05-04 23:20:33 +0200

    Getting started with animated lists

---
+++ animations-01-starting-setup/src/components/UserList.vue
@@ -0,0 +1,49 @@
+<template>
+    <ul>
+        <li v-for="user in users" :key="user" @click="removeUser(user)">{{ user }}</li>
+    </ul>
+    <div>
+        <input type="text" v-model="newUser"/>
+        <button @click="addUser">Add User</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            newUser: '',
+            users: [
+                'Ana',
+                'Larson',
+                'Jerry',
+                'Jean', 
+                'Manuel'
+            ]
+        };
+    },
+    methods: {
+        addUser() {
+            this.users.push(this.newUser);
+            this.newUser = '';
+        },
+        removeUser(user) {
+            this.users.splice(this.users.findIndex((currUser) => currUser === user), 1);
+        }
+    }
+}
+</script>
+
+<style scoped>
+ul {
+    list-style: none;
+    margin: 1rem 0;
+    padding: 0;
+}
+
+li {
+    border: 1px solid #ccc;
+    padding: 1rem;
+    text-align: center;
+}
+</style>
