2097999de1d97c5387d52c1900b0bc7f8c1976a3
Arthur (DPA) <a.foadjo-dje@psychologenakademie.de>
2024-05-05 11:17:48 +0200

    Starting project + creating and using store

---
+++ vuex-01-starting-setup/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <base-container title="Vuex">
+    <div>
+      <h3>{{ $store.state.counter }}</h3>
+      <button @click="increment">Add 1</button>
+    </div>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+
+export default {
+  components: {
+    BaseContainer,
+  },
+  methods: {
+    increment() {
+      this.$store.state.counter++;
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
