f41996cd9903126239caf553bef14dd95f76ab8a
Arthur (DPA) <a.foadjo-dje@psychologenakademie.de>
2024-05-25 18:26:23 +0200

    Working on the CoachDetail component

--- coach-finder/src/pages/coaches/CoachDetail.vue
+++ coach-finder/src/pages/coaches/CoachDetail.vue
@@ -1,7 +1,56 @@
 <template>
-  Details for Coach
-  <router-view></router-view>
-  <router-link to="/coaches/c1/contact">Contact</router-link>
+  <section>
+    <base-card>
+      <h2>{{ fullName }}</h2>
+      <h3>${{ rate }}/hour</h3>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <header>
+        <h2>Interested? Reach out now!</h2>
+        <base-button :link="true" :to="coantactLink">Contact</base-button>
+      </header>
+      <router-view></router-view>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <base-badge v-for="area in areas" :key="area" :type="area" :text="area"></base-badge>
+      <p>{{ description }}</p>
+    </base-card>
+  </section>
 </template>
 
-<script></script>
+<script>
+export default {
+  props: ['id'],
+  data() {
+    return {
+      selectedCoach: null
+    }
+  },
+  computed: {
+    areas() {
+      return this.selectedCoach.areas
+    },
+    constactLink() {
+      return this.$route.path + '/' + this.id + '/contact'
+    },
+    description() {
+      return this.selectedCoach.description
+    },
+    fullName() {
+      return this.selectedCoach.firstName + ' ' + this.selectedCoach.lastName
+    },
+    rate() {
+      return this.selectedCoach.hourlyRate
+    }
+  },
+  created() {
+    this.selectedCoach = this.$store.getters['coaches/coaches'].find(
+      (currCoach) => currCoach.id === this.id
+    )
+  }
+}
+</script>
