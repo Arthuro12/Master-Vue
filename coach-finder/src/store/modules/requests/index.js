faf3528578adfe26d181a23e214726651009b133
Arthur (DPA) <a.foadjo-dje@psychologenakademie.de>
2024-06-27 23:31:27 +0200

    Storing requests with Vuex

--- coach-finder/src/store/modules/requests/index.js
+++ coach-finder/src/store/modules/requests/index.js
@@ -1,1 +1,13 @@
-export default {}
+import actions from './actions.js'
+import mutations from './mutations.js'
+
+export default {
+  namespaced: true,
+  state() {
+    return {
+      requests: []
+    }
+  },
+  mutations,
+  actions
+}
