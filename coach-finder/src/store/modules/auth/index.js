e5782c6f8227e6b1d109122b8a7e3d0f493ca505
Arthur (DPA) <a.foadjo-dje@psychologenakademie.de>
2024-07-13 22:56:49 +0200

    Prepare Vuex for authentication module

---
+++ coach-finder/src/store/modules/auth/index.js
@@ -0,0 +1,14 @@
+import actions from './actions.js'
+import getters from './getters.js'
+import mutations from '../coaches/mutations.js'
+
+export default {
+  sate() {
+    return {
+      userId: 'c3'
+    }
+  },
+  actions,
+  getters,
+  mutations
+}
