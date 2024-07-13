893199f2e637fc1234a0964898069fa1af060ed8
Arthur (DPA) <a.foadjo-dje@psychologenakademie.de>
2024-07-13 14:28:50 +0200

    Fill the not found page

--- coach-finder/src/pages/NotFound.vue
+++ coach-finder/src/pages/NotFound.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <section></section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        This page could not be found - maybe check out al our
+        <router-link to="/coaches">coaches</router-link>
+      </p>
+    </base-card>
   </div>
 </template>
 
