cf2ba1019494973ac50789322141bdc6497b9bf4
Arthuro12 <arthurfoadjo@yahoo.com>
2024-07-15 13:10:35 +0200

    Add "Auto Login"

--- coach-finder/src/store/modules/auth/actions.js
+++ coach-finder/src/store/modules/auth/actions.js
@@ -1,19 +1,22 @@
 const API_KEY = 'AIzaSyDCDZpOC7X5Od2yyuAOkxq1a0BIukyTLQA'
 
 export default {
-  async login(context, payload) {
+  async auth(context, payload) {
+    const mode = payload.mode
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`
+
+    if (mode === 'signup') {
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`
+    }
     try {
-      const response = await fetch(
-        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`,
-        {
-          method: 'POST',
-          body: JSON.stringify({
-            email: payload.email,
-            password: payload.password,
-            returnSecureToken: true
-          })
-        }
-      )
+      const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true
+        })
+      })
 
       if (!response.ok) {
         const error = new Error(
@@ -23,7 +26,10 @@ export default {
       }
 
       const responseData = await response.json()
-      console.log(responseData)
+
+      localStorage.setItem('token', responseData.idToken)
+      localStorage.setItem('userId', responseData.localId)
+
       context.commit('setUser', {
         token: responseData.idToken,
         userId: responseData.localId,
@@ -33,37 +39,29 @@ export default {
       throw error
     }
   },
-  async signup(context, payload) {
-    try {
-      const response = await fetch(
-        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
-        {
-          method: 'POST',
-          body: JSON.stringify({
-            email: payload.email,
-            password: payload.password,
-            returnSecureToken: true
-          })
-        }
-      )
+  tryLogin(context) {
+    const token = localStorage.getItem('token')
+    const userId = localStorage.getItem('userId')
 
-      if (!response.ok) {
-        const error = new Error(
-          response.statusText || 'Failed to authenticate. Check your login data.'
-        )
-        throw error
-      }
-
-      const responseData = await response.json()
-      console.log(responseData)
+    if (token && userId) {
       context.commit('setUser', {
-        token: responseData.idToken,
-        userId: responseData.localId,
-        tokenExpiration: responseData.expiresIn
+        token,
+        userId,
+        tokenExpiration: null
       })
-    } catch (error) {
-      throw error
     }
+  },
+  async login(context, payload) {
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'login'
+    })
+  },
+  async signup(context, payload) {
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signup'
+    })
   },
   logout(context) {
     context.commit('setUser', {
