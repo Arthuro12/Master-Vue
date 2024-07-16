f0f2962d35314f95bda65e4cedb798a90068c360
Arthuro12 <arthurfoadjo@yahoo.com>
2024-07-16 02:47:51 +0200

    Deeping dive into reactivty

--- composition-01-starting-setup/src/App.vue
+++ composition-01-starting-setup/src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <section class="container">
-    <h2>{{ user.name }}</h2>
-    <h3>{{ user.age }}</h3>
+    <h2>{{ name }}</h2>
+    <h3>{{ age }}</h3>
   </section>
 </template>
 
 <script>
-import { reactive, ref } from 'vue';
+import { reactive, ref, toRefs } from 'vue';
 
 export default {
   setup() {
@@ -21,18 +21,31 @@ export default {
       age: 45,
     });
 
+    const user2 = reactive({
+      name: 'Marco',
+      age: 27,
+    });
+
+    const user2Ref = toRefs(user2);
+
+    // console.log(user, user2);
+
     setTimeout(() => {
       //userName.value = 'Billie';
       // user.value.name = 'Billie';
       // user.value.age = 21;
-      user.name = 'Nathy';
-      user.age = 25;
+      // user.name = 'Nathy';
+      // user.age = 25;
+      user2.name = 'Nathy';
+      user2.age = 25;
     }, 2000);
 
     return {
       // userName,
       // age,
-      user,
+      // user,
+      name: user2Ref.name,
+      age: user2Ref.age,
     };
   },
 };
