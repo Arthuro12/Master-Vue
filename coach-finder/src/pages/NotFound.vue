9a550c902856c803d64da450ff20ad145c489fbf
Arthuro12 <arthurfoadjo@yahoo.com>
2024-07-21 21:32:44 +0200

    Fix tipo

--- coach-finder/src/pages/NotFound.vue
+++ coach-finder/src/pages/NotFound.vue
@@ -3,7 +3,7 @@
     <base-card>
       <h2>Page not found</h2>
       <p>
-        This page could not be found - maybe check out al our
+        This page could not be found - maybe check out all our
         <router-link to="/coaches">coaches</router-link>
       </p>
     </base-card>
